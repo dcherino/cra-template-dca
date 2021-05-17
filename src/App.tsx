ef52aeef3cc3fc30a0dfaea05e3fd9f6164f8e86
Daniel Cherino <dan.cherino@doctorcareanywhere.com>
2021-05-17 11:39:23 +0100

    Adding style to default page

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,29 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './globalStyle';
 import theme from './theme';
+import { AppWrap, Header, Main, Footer } from './App.styles';
 import logo from './logo.svg';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div className="App">
-        <h1>Create React App</h1>
-        <img src={logo} alt="React logo" />
-      </div>
+      <AppWrap>
+        <Header>
+          <h1>Create React App DCA</h1>
+        </Header>
+        <Main>
+          <img src={logo} className="logo" alt="React logo" />
+        </Main>
+        <Footer>
+          <p>
+            An opinionated quick start{' '}
+            <a href="https://github.com/facebook/create-react-app">
+              Create React App
+            </a>
+          </p>
+        </Footer>
+      </AppWrap>
     </ThemeProvider>
   );
 }
